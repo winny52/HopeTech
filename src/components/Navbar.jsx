153da53523f46ad739a0fb1594bc4dc6e91131ea
Winny Chelangat <winnychelangat28@gmail.com>
2024-06-11 20:45:45 +0300

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,71 +9,76 @@ const Navbar = () => {
   };
 
   return (
-    <nav className=" bg-white p-5 pb- px-8 text-blue-900 rounded-b-2xl text-lg border-b-2  border-blue-100">
-      <div className="container mx-aut0 flex justify-betwee gap-96 items-center">
+    <nav className=" p-8 mx-3 text-blue-600 rounded-2xl  border-b-2 border-b-2 border-blue-100">
+      <div className="container mx-aut0 flex justify-between items-center">
         <span className=" font-semibold text-lg">Logo</span>
         <button className=" text-xl md:hidden" onClick={toggleNavbar}>
           {isOpen ? 'Close' : 'Menu'}
         </button>
-        <ul className={`md:flex md:items-center space-7  ${isOpen ? 'block' : 'hidden'}`}>
+        <ul className={`md:flex md:items-center space-7 ${isOpen ? 'block' : 'hidden'}`}>
           <li className=" ">
-            <Link to="/" className=" hover:text-blue-400 mx-4 rounded p-8">
+            <Link to="/" className=" hover:bg-gray-700 px-3 py-2 rounded ">
               Home
             </Link>
           </li>
           <li className="relative group">
-            <span className=" hover:text-blue-400 mx-4 rounded pb-8 cursor-pointer">
+            <span className=" hover:bg-gray-700 px-3 py-2 rounded cursor-pointer">
               About Us
             </span>
-            <ul className="absolute left-0 mt-1 hidden w-48 bg-white mt-6 rounded-b-2xl group-hover:block">
+            <ul className="absolute left-0 mt-1 hidden w-48 bg-gray-800 group-hover:block">
               <li>
-                <Link to="/about" className="block  hover:text-blue-400 mx-4 rounded ">
+                <Link to="/about" className="block  hover:bg-gray-700 px-3 py-2 rounded">
                   About Us
                 </Link>
               </li>
               <li>
-                <Link to="/about/why-choose-us" className="block  hover:text-blue-400 mx-4 rounded ">
+                <Link to="/about/why-choose-us" className="block  hover:bg-gray-700 px-3 py-2 rounded">
                   Why Choose Us
                 </Link>
               </li>
-            </ul>
+                          </ul>
           </li>
           <li className="relative group">
-            <span className=" hover:text-blue-400 mx-4 rounded p-8 cursor-pointer">
+            <span className=" hover:bg-gray-700 px-3 py-2 rounded cursor-pointer">
               Our Services
             </span>
-            <ul className="absolute left-0  hidden w-48 mt-6 bg-white rounded-b-2xl group-hover:block">
+            <ul className="absolute left-0 mt-1 hidden w-48 bg-gray-800 group-hover:block">
               <li>
-                <Link to="/services/solutions" className="block hover:text-blue-400 mx-4 rounded ">
-                  Solutions
+                <Link to="/services/solutions" className="block  hover:bg-gray-700 px-3 py-2 rounded">
+                  Process
                 </Link>
               </li>
               <li>
-                <Link to="/services/services" className="block  hover:text-blue-400 mx-4 rounded ">
+                <Link to="/services/services" className="block  hover:bg-gray-700 px-3 py-2 rounded">
                   Services
                 </Link>
               </li>
             </ul>
           </li>
           <li className="relative group">
-            <span className=" hover:text-blue-400 mx-4 p-8  rounded cursor-pointer">
+            <span className=" hover:bg-gray-700 px-3 py-2 rounded cursor-pointer">
               Portfolio
             </span>
-            <ul className="absolute left-0 mt-6 bg-white rounded-b-2xl hidden w-48  group-hover:block">
-              <li>
-                <Link to="/portfolio/testimonials" className="block  hover:text-blue-400 mx-4 rounded ">
+            <ul className="absolute left-0 mt-1 hidden w-48 bg-gray-800 group-hover:block">
+            <li>
+                <Link to="/portfolio/testimonials" className="block  hover:bg-gray-700 px-3 py-2 rounded">
                   Testimonials
                 </Link>
               </li>
               <li>
-                <Link to="/portfolio/our-work" className="block  hover:text-blue-400 mx-4 rounded ">
+                <Link to="/portfolio/our-work" className="block  hover:bg-gray-700 px-3 py-2 rounded">
                   Our Work
                 </Link>
               </li>
             </ul>
           </li>
           <li>
-            <Link to="/contact" className=" hover:text-blue-400 mx-4 rounded ">
+            <Link to="/faqs" className="text-white hover:bg-gray-700 px-3 py-2 rounded">
+              FAQs
+            </Link>
+            </li>
+          <li>
+            <Link to="/contact" className="text-white hover:bg-gray-700 px-3 py-2 rounded">
               Contact Us
             </Link>
           </li>
